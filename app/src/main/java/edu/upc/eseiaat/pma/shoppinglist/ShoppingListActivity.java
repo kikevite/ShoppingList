e47119d6556aa8cd278768af7916b9ad88e5e012
kikevite <brianbvf@gmail.com>
2017-10-20 13:27:23 +0200

    Commit apartat 5. Layout creat i referencies als elements fetes

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -2,12 +2,23 @@ package edu.upc.eseiaat.pma.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class ShoppingListActivity extends AppCompatActivity {
+
+    private ListView list;
+    private Button btn_add;
+    private EditText edit_item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+
+        list = (ListView) findViewById(R.id.list);
+        btn_add = (Button) findViewById(R.id.btn_add);
+        edit_item = (EditText) findViewById(R.id.edit_item);
     }
 }
