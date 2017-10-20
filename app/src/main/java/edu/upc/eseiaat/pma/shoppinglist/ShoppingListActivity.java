ec5cf81cfa73bfcb4b7f0d60c6b536c403c6c0c3
kikevite <brianbvf@gmail.com>
2017-10-20 19:24:04 +0200

    Commit apartat 9. S'afegeixen items amb el button i amb el teclat.

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -2,10 +2,13 @@ package edu.upc.eseiaat.pma.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -35,6 +38,33 @@ public class ShoppingListActivity extends AppCompatActivity {
 
         adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, itemList);
 
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItem();
+            }
+        });
+
+        edit_item.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                addItem();
+                return true;
+            }
+        });
+
         list.setAdapter(adapter);
+    }
+
+    private void addItem() {
+        String item_text = edit_item.getText().toString();
+        if (!item_text.isEmpty()) {
+            itemList.add(item_text);
+            adapter.notifyDataSetChanged();
+            edit_item.setText("");
+            ///Fet fins a 32.06
+        }
+
+
     }
 }
