a73b8860925fad6c6f45e1ec032bed5925657d84
kikevite <brianbvf@gmail.com>
2017-10-27 13:27:00 +0200

    Video 3: Commit apartat 3.

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -1,18 +1,23 @@
 package edu.upc.eseiaat.pma.shoppinglist;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ShoppingListActivity extends AppCompatActivity {
@@ -23,6 +28,33 @@ public class ShoppingListActivity extends AppCompatActivity {
     private ListView list;
     private Button btn_add;
     private EditText edit_item;
+
+    private static final String FILENAME = "shopping_list.txt";
+    private void writeItemList(){
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            for (int i=0; i<itemList.size(); i++){
+                ShoppingItem it = itemList.get(i);
+                String line = String.format("%s;%b\n", it.getText(), it.isChecked());
+                fos.write(line.getBytes());
+            }
+            fos.close();
+        } catch (FileNotFoundException e) {
+            Log.i("kike", "writeItemList: FileNotFoundException");
+            Toast.makeText(this, R.string.cannot_write, Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Log.i("kike", "writeItemList: IOException");
+            Toast.makeText(this, R.string.cannot_write, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        writeItemList();
+        Log.i("kike", "writeItemList: File writted!");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
